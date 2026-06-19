c8f59703a62a4776c0fc1d34fa75e7ada879bb31
MohammedAbdul20 <mohdabdulilla786@gmail.com>
2026-06-19 12:47:33 +0530

    Exercise C# code

---
+++ C#practice/Exercises/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("What kind of thing are we talking about?");
+string? a = Console.ReadLine();//Stores a thing 
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string? b = Console.ReadLine();//Stores a Adjective 
+/* this variable here adn below it stores words like doom and 3000*/
+string c = "Doom";
+string d = "3000";
+Console.WriteLine("The " + b + " " + a + " of " + c + " " + d + "!");
