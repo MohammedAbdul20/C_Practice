17ab84d87eec59bfdbb23da4669a49a31eb8f54a
MohammedAbdul20 <mohdabdulilla786@gmail.com>
2026-06-22 11:01:12 +0530

    different datatypes

---
+++ C#practice/Exercise3/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Hello, World!");
+
+byte byteNumber = 24;
+Console.WriteLine("A ByteNumber " + byteNumber);
+
+sbyte sbyteNumber = -12;
+Console.WriteLine("A Signed ByteNumber " + sbyteNumber);
+
+short shortNumber = 23786;
+Console.WriteLine("A shortNumber " + shortNumber);
+
+int intNumber = 1_500_000;
+Console.WriteLine("A intNumber " + intNumber);
+
+long longNumber = 15_000_000_000;
+Console.WriteLine("A longNumber " + longNumber);
+
+uint uIntNumber = 3_500_000;
+Console.WriteLine("An unsigned intNumber " + uIntNumber);
+
+ushort uShortNumber = 57_543;
+Console.WriteLine("An unsigned shortNumber " + uShortNumber);
+
+ulong uLongNumber = 650_000_000_000_000;
+Console.WriteLine("An unsigned longNumber " + uLongNumber);
+
+char aCharacter = 'I';
+Console.WriteLine("A character " + aCharacter);
+
+string Name = "Travis Scott";
+Console.WriteLine("A String " + Name);
+
+float aFloatNumber = 450.45f;
+Console.WriteLine("A floatNUmber " + aFloatNumber);
+
+double aDoubleNumber = 45.9864822;
+Console.WriteLine("A doubleNumber " + aDoubleNumber);
+
+decimal aDecimalNumber = 67.82648202m;
+Console.WriteLine("A decimalNumber " + aDecimalNumber);
+
+bool isMan = true;
+Console.WriteLine("A boolValue " + isMan);
+
