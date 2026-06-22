e8c885ef393f3441743ea52371796035f9369191
MohammedAbdul20 <mohdabdulilla786@gmail.com>
2026-06-22 11:51:56 +0530

    changed the code to accept values dynamically

--- C#practice/Exercise3/Program.cs
+++ C#practice/Exercise3/Program.cs
@@ -1,44 +1,57 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("Hello, World!");
 
-byte byteNumber = 24;
-Console.WriteLine("A ByteNumber " + byteNumber);
+Console.WriteLine("Enter a byte Number: ");
+byte byteNumber = Convert.ToByte(Console.ReadLine());
+Console.WriteLine("A ByteNumber: " + byteNumber);
 
-sbyte sbyteNumber = -12;
-Console.WriteLine("A Signed ByteNumber " + sbyteNumber);
+Console.WriteLine("Enter a signed byte Number");
+sbyte sbyteNumber = Convert.ToSByte(Console.ReadLine());
+Console.WriteLine("A Signed ByteNumber: " + sbyteNumber);
 
-short shortNumber = 23786;
-Console.WriteLine("A shortNumber " + shortNumber);
+Console.WriteLine("Enter a short Number");
+short shortNumber = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("A shortNumber: " + shortNumber);
 
-int intNumber = 1_500_000;
-Console.WriteLine("A intNumber " + intNumber);
+Console.WriteLine("Enter an int Number");
+int intNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("An intNumber: " + intNumber);
 
-long longNumber = 15_000_000_000;
-Console.WriteLine("A longNumber " + longNumber);
+Console.WriteLine("Enter a long Number");
+long longNumber = Convert.ToInt64(Console.ReadLine());
+Console.WriteLine("A longNumber: " + longNumber);
 
-uint uIntNumber = 3_500_000;
-Console.WriteLine("An unsigned intNumber " + uIntNumber);
+Console.WriteLine("Enter an unsigned int Number");
+uint uIntNumber = Convert.ToUInt32(Console.ReadLine());
+Console.WriteLine("An unsigned intNumber: " + uIntNumber);
 
-ushort uShortNumber = 57_543;
-Console.WriteLine("An unsigned shortNumber " + uShortNumber);
+Console.WriteLine("Enter an unsigned short Number");
+ushort uShortNumber = Convert.ToUInt16(Console.ReadLine());
+Console.WriteLine("An unsigned shortNumber: " + uShortNumber);
 
-ulong uLongNumber = 650_000_000_000_000;
-Console.WriteLine("An unsigned longNumber " + uLongNumber);
+Console.WriteLine("Enter an unsigned long Number");
+ulong uLongNumber = Convert.ToUInt64(Console.ReadLine());
+Console.WriteLine("An unsigned longNumber: " + uLongNumber);
 
-char aCharacter = 'I';
-Console.WriteLine("A character " + aCharacter);
+Console.WriteLine("Enter a Charater");
+char aCharacter = Convert.ToChar(Console.ReadLine());
+Console.WriteLine("A character: " + aCharacter);
 
-string Name = "Travis Scott";
-Console.WriteLine("A String " + Name);
+Console.WriteLine("Enter a new string");
+string Name = Console.ReadLine();
+Console.WriteLine("A String: " + Name);
 
-float aFloatNumber = 450.45f;
-Console.WriteLine("A floatNUmber " + aFloatNumber);
+Console.WriteLine("Enter a float Number");
+float aFloatNumber = Convert.ToSingle(Console.ReadLine());
+Console.WriteLine("A floatNUmber: " + aFloatNumber);
 
-double aDoubleNumber = 45.9864822;
-Console.WriteLine("A doubleNumber " + aDoubleNumber);
+Console.WriteLine("Enter a Double Number");
+double aDoubleNumber = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("A doubleNumber: " + aDoubleNumber);
 
-decimal aDecimalNumber = 67.82648202m;
-Console.WriteLine("A decimalNumber " + aDecimalNumber);
+Console.WriteLine("Enter a decimal Number");
+decimal aDecimalNumber = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("A decimalNumber: " + aDecimalNumber);
 
-bool isMan = true;
-Console.WriteLine("A boolValue " + isMan);
-
+Console.WriteLine("Enter a Bool Value");
+bool isMan = Convert.ToBoolean(Console.ReadLine());
+Console.WriteLine("A boolValue: " + isMan);
