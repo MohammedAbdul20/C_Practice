257e9e229959614ddbc40d1227873464ab95ed81
MohammedAbdul20 <mohdabdulilla786@gmail.com>
2026-07-10 18:37:51 +0530

    adding code as backup

---
+++ C#practice/Exercise2/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        Console.WriteLine("What kind of thing are we talking about?");
+        string a = Console.ReadLine();
+        Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+        string b = Console.ReadLine();
+        string c = "of Doom";
+        string d = "3000";
+        Console.WriteLine("The " + b + " " + a + " of " + c + " " + d + "!");
+    }
+}
