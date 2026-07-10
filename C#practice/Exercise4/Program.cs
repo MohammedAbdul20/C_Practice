7eb53dc28b44d36d7cf30f5bcc0a5e89782f77ed
MohammedAbdul20 <mohdabdulilla786@gmail.com>
2026-07-10 18:35:42 +0530

    did some exercise

---
+++ C#practice/Exercise4/Program.cs
@@ -0,0 +1,24 @@
+﻿
+////Area of the Triangle Program.
+//Console.WriteLine("Enter the base of the triangle: ");
+//double Base = Convert.ToDouble(Console.ReadLine());
+
+//Console.WriteLine("Enter the height of the Triangle: ");
+//double Height = Convert.ToDouble(Console.ReadLine());
+
+//double Area = (Base * Height) / 2;
+
+//Console.WriteLine("The area of the triangle is: " +  Area);
+
+//int a = 5;
+//int b = 2;
+//int result = a / b;
+//Console.WriteLine(result);
+
+Console.WriteLine("enter the no of chocolates: ");
+int chocolates = Convert.ToInt32(Console.ReadLine());
+
+int eachPersonChocoShare = chocolates / 4;
+float chocolatesForPet = chocolates % 4;
+
+Console.WriteLine("The chcolates for each person is: " + eachPersonChocoShare + " and choco for pet is: " + chocolatesForPet);
