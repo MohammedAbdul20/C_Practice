3cba66656417a3166bddd79ff825128ea6999781
MohammedAbdul20 <mohdabdulilla786@gmail.com>
2026-07-10 18:41:58 +0530

    backup

---
+++ C#practice/MyFirstProgram/MyFirstProgram/Exercise1.cs
@@ -0,0 +1,15 @@
+﻿
+namespace MyFirstProgram
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Bread is ready.");
+            Console.WriteLine("Who is the bread for? ");
+            string? Name;
+            Name = Console.ReadLine();
+            Console.WriteLine("Noted: " + Name + " got bread.");
+        }
+    }
+}
---
+++ C#practice/MyFirstProgram/MyFirstProgram/Program.cs
@@ -0,0 +1,9 @@
+﻿////Console.WriteLine("Boom! Boom! Boom! Boom! Boom! BOOM!");
+////Console.WriteLine("Boom! Boom! Boom! Boom! Boom! BOOM!");
+
+
+//Console.WriteLine("What's your Name?");
+//string name;
+//name = Console.ReadLine();
+//Console.WriteLine("Hi " + name);
+
